0435e772a8ea1bb384b05db2cecf8c8887b7dc87
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-05-28 04:10:41 -0400

    fix: total users

--- components/Manage/index.tsx
+++ components/Manage/index.tsx
@@ -11,20 +11,24 @@ import { DMSans400 } from '../../utils/fonts';
 const SDKDemo = () => {
 
   
+
+
   const calculateData = (transactions) => {
 
     // total data
-    let totalUsers = new Set();
-    const totalTxns = transactions.length
-
+    let totalUsers = new Set(); 
+    const totalTxns = transactions.length;
+  
     // todays data
     const today = new Date();
-    let todaysTxns = []
-    let todaysUsers = new Set();
-    let todaysTxnNumber = 0
-
+    let todaysTxns = [];
+    let todaysUsers = new Set(); 
+    let todaysTxnNumber = 0;
+  
     transactions.forEach((transaction) => {
       const transactionDate = new Date(transaction.date);
+      // total
+      totalUsers.add(transaction.userID);
       // today
       if (
         today.getDate() === transactionDate.getDate() &&
@@ -32,22 +36,20 @@ const SDKDemo = () => {
         today.getFullYear() === transactionDate.getFullYear()
       ) {
         todaysTxns.push(transaction);
-        todaysUsers.add(transaction.userID)
-        todaysTxnNumber++
+        todaysUsers.add(transaction.userID);
+        todaysTxnNumber++;
       } 
-      // total
-      else {
-        totalUsers.add(transaction.userID);
-      }
-
-
     });
+  
     return { 
-      totalTxns, totalUsers: totalUsers.size,
-      todaysUsers: todaysUsers.size, todaysTxns, todaysTxnNumber
+      totalTxns,
+      totalUsers: totalUsers.size, 
+      todaysUsers: todaysUsers.size,
+      todaysTxns,
+      todaysTxnNumber
     };
-
   };
+  
 
 
 
